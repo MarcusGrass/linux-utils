18e9c3edf3cc4888f0f3efee2bed57a5ad27c4e3
gramar <marcus.grass@gmail.com>
2022-08-18 13:07:56 +0200

    Fix script

--- sys-cfg/lenovo/install_arch.sh
+++ sys-cfg/lenovo/install_arch.sh
@@ -11,4 +11,4 @@ cryptsetup close cswap
 cryptsetup close chome
 cryptsetup close croot
 # Ready to run
-curl -L https://github.com/MarcusGrass/linux-utils/blob/main/arch-installer-bin?raw=true -o /tmp/arch-installer-bin && chmod +x /tmp/arch-installer-bin && /tmp/arch-installer-bin --efi-device-name nvme0n1 --efi-device-root nvme0n1p1 --home-device-crypt-name chome --home-device-name nvme0n1p4 --home-device-root nvme0n1 --root-device-crypt-name croot --root-device-name nvme0n1p2 --root-device-root nvme0n1 --swap-device-name nvme0n1p3 --swap-device-root nvme0n1 --swap-device-crypt-name cswap
+curl -L https://github.com/MarcusGrass/linux-utils/blob/main/arch-installer-bin?raw=true -o /tmp/arch-installer-bin && chmod +x /tmp/arch-installer-bin && /tmp/arch-installer-bin stage1 --efi-device-name nvme0n1 --efi-device-root nvme0n1p1 --home-device-crypt-name chome --home-device-name nvme0n1p4 --home-device-root nvme0n1 --root-device-crypt-name croot --root-device-name nvme0n1p2 --root-device-root nvme0n1 --swap-device-name nvme0n1p3 --swap-device-root nvme0n1 --swap-device-crypt-name cswap
