1a85888c07ac61ce81e6e4f5f95b3f2abaa2cdc4
gramar <marcus.grass@gmail.com>
2022-08-19 15:23:27 +0200

    run chpasswd

--- sys-cfg/lenovo/install_arch.sh
+++ sys-cfg/lenovo/install_arch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Ignores errors
 # Try delete old, doesn't matter if exists
-# Dummy refresh token
+# Dummy refresh
 rm /tmp/arch-installer-bin
 # Try unmount old, doesn't matter if not mounted
 umount /mnt/home
