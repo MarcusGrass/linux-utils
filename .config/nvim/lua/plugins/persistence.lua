e7075bbf9588ccb4ea1dd7d99472938e61d3f38b
MarcusGrass <marcus.grass@protonmail.com>
2025-02-01 00:02:12 +0100

    Add persistence

---
+++ .config/nvim/lua/plugins/persistence.lua
@@ -0,0 +1,11 @@
+local cfg = function(_, opts)
+    vim.keymap.set("n", "<leader>qS", function()
+        require("persistence").select()
+    end)
+    require("persistence").setup({})
+end
+return {
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    config = cfg,
+}
--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -8,7 +8,39 @@ return {
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
         bigfile = { enabled = false },
-        dashboard = { enabled = false },
+        dashboard = {
+            enabled = true,
+            sections = {
+                { section = "header" },
+                { section = "keys", gap = 1, padding = 1 },
+                { section = "startup" },
+                {
+                    pane = 2,
+                    icon = " ",
+                    key = "s",
+                    title = "Sessions",
+                    section = "session",
+                    action = ":lua require('persistence').select()",
+                    indent = 2,
+                    padding = 1,
+                },
+                { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+                {
+                    pane = 2,
+                    icon = " ",
+                    title = "Git Status",
+                    section = "terminal",
+                    enabled = function()
+                        return require("snacks").git.get_root() ~= nil
+                    end,
+                    cmd = "git status --short --branch --renames",
+                    height = 5,
+                    padding = 1,
+                    ttl = 5 * 60,
+                    indent = 3,
+                },
+            },
+        },
         indent = { enabled = false },
         input = { enabled = true },
         picker = {
