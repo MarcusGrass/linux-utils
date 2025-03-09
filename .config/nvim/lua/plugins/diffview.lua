2d53b23d1b7e482d26f7c3d2228e547af251ad9d
MarcusGrass <marcus.grass@protonmail.com>
2025-03-09 14:45:40 +0100

    Remove some unused commands after removing fugitive

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -34,16 +34,6 @@ return {
                         require("diffview.config").actions.goto_file_edit,
                         { desc = "Goto file in last accessed tab" },
                     },
-                    { "n", "<leader>cf", "<CMD>Git commit <bar> wincmd J<CR>", { desc = "Commit file" } },
-                    {
-                        "n",
-                        "<leader>ca",
-                        "<CMD>Git commit --amend <bar> wincmd J<CR>",
-                        { desc = "Amend last commit with changes in file" },
-                    },
-                    -- These are going to split the window, Ideally it'll split to the bottom
-                    { "n", "<leader>ccs", ":botright Git commit<CR>", { desc = "Commit all staged" } },
-                    { "n", "<leader>cca", ":botright Git commit --amend<CR>", { desc = "Commit all staged, amend" } },
                 },
                 file_history_panel = {
                     {
